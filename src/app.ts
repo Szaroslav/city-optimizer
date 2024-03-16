5c4d09144d4cc83df5dc0e48c0f07657f6d59991
Jakub Szaredko <54867641+Szaroslav@users.noreply.github.com>
2024-03-16 15:57:35 +0100

    Calendarific: Collect necessary data (#8)

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,66 @@
 import Express from "express";
 import "dotenv/config";
 
+import { Calendarific } from "./api/Calendarific";
+import { CalendarificHolidays } from "./models/calendarific/CalendarificHolidays";
+
+const { CALENDARIFIC_API_KEY } = process.env;
+if (CALENDARIFIC_API_KEY == undefined) {
+  throw new Error("Calendarific API key is not defined");
+}
+const { CALENDARIFIC_HOLIDAYS_URL } = process.env;
+if (CALENDARIFIC_HOLIDAYS_URL == undefined) {
+  throw new Error("Calendarific holidays route URL is not defined");
+}
+const { CALENDARIFIC_COUNTRIES_URL } = process.env;
+if (CALENDARIFIC_COUNTRIES_URL == undefined) {
+  throw new Error("Calendarific countries route URL is not defined");
+}
 const PORT = process.env.PORT ?? 9333;
 
 const app = Express();
+const calendarific = new Calendarific(
+  CALENDARIFIC_API_KEY,
+  CALENDARIFIC_COUNTRIES_URL,
+  CALENDARIFIC_HOLIDAYS_URL);
 
 app.get("/", (_, res) => {
   res.send("Pot Holding");
+});
+
+app.get("/v1/country-calendar-info", async (req, res) => {
+  const { countryId } = req.query;
+  if (!countryId || typeof countryId !== "string") {
+    let message: string;
+    if (!countryId) {
+      message = "Country ID is not defined";
+    }
+    else {
+      message = "Country ID is not a string";
+    }
+
+    res.status(400);
+    res.send({ message });
+    return;
+  }
+
+  try {
+    const holidaysDto = await calendarific.fetchHolidays(countryId),
+          holidays    = new CalendarificHolidays(countryId, holidaysDto);
+
+    res.send(holidays);
+  }
+  catch (error) {
+    res.status(400);
+    if (error instanceof Error) {
+      res.send({ message: error.message });
+    }
+    else {
+      res.send({ message: "Something went wrong" });
+    }
+  }
+
+  res.send();
 });
 
 app.listen(PORT, () => {
---
+++ src/models/calendarific/CalendarificHolidays.ts
@@ -0,0 +1,30 @@
+import {
+  CalendarificHolidaysDto,
+  HolidayType
+} from "../../dtos/calendarific/CalendarificHolidaysDto";
+
+export class CalendarificHolidays {
+  public countryId: string;
+  public holidays:  Holiday[];
+
+  constructor(countryId: string, dto: CalendarificHolidaysDto) {
+    this.countryId = countryId;
+    this.holidays  = this.getHolidaysFromDto(dto);
+  }
+
+  private getHolidaysFromDto(dto: CalendarificHolidaysDto): Holiday[] {
+    return dto.response.holidays.map((holiday => ({
+      name: holiday.name,
+      description: holiday.description,
+      date: new Date(holiday.date.iso),
+      type: holiday.primary_type,
+    })));
+  }
+}
+
+interface Holiday {
+  name:        string;
+  description: string;
+  date:        Date;
+  type:        HolidayType;
+}
