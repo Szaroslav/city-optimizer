4d4f41da58ab090858b6350d045fdd5497f6cbc4
Jakub Szaredko <54867641+Szaroslav@users.noreply.github.com>
2024-03-17 13:29:36 +0100

    Templates: Create the error template (#16)

--- src/api/FetchService.ts
+++ src/api/FetchService.ts
@@ -1,8 +1,11 @@
+import { ServerError } from "../ServerError";
+
 export class FetchService {
   protected async fetchJson<T>(url: string): Promise<T> {
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error("Fetch error: " + response.statusText);
+      throw new ServerError(
+        response.status, "Fetch error: " + response.statusText);
     }
     return response.json();
   }
